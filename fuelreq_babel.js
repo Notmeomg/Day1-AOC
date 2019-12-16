6a517fb9ad8dba7a5ce21fefdb2de1fa2d6877a4
unknown <Zachariahkeener@gmail.com>
2019-12-16 11:23:27 -0700

    babel

---
+++ fuelreq_babel.js
@@ -0,0 +1,12 @@
+import { arr1, arr2 } from './mass_babel';
+// req flag --experimental-modules and file ext .mjs
+// to ignore flag use Typescript, Babel, Rollup
+// cannot use require in .mjs files however you load both kinds of modules in .mjs
+
+// export default
+// console.log(arr);
+
+// export const arr
+// console.log(arr);
+
+console.log(arr1, arr2);
