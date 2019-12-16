13ddbb19496070d1faf264db40a9539415753e69
unknown <Zachariahkeener@gmail.com>
2019-12-16 10:08:32 -0700

    day1

--- fuelreq.js
+++ fuelreq.js
@@ -1,3 +1,15 @@
-import mass from './mass';
+const mass = require('./mass');
 
+// exports.arr
+// console.log(marr.arr);
+
+// exports.arr = arr;
+// console.log(mass.arr);
+
+// module.exports
+// console.log(mass);
+
+// module.exports = {}
 console.log(mass);
+console.log(mass.arr1);
+console.log(mass.arr2);
---
+++ fuelreq.mjs
@@ -0,0 +1,13 @@
+import { arr1, arr2 } from './mass.mjs';
+// req flag --experimental-modules and file ext .mjs
+// to ignore flag use Typescript, Babel, Rollup
+// cannot use require in .mjs files however you load both kinds of modules in .mjs
+
+// export default
+// console.log(arr);
+
+// export const arr
+// console.log(arr);
+
+// export {}
+// console.log(arr1, arr2);
